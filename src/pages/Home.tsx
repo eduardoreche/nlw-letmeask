0dc13c1a45c213a8f0ddcf63d1c8ca5fa5a51090
Eduardo Reche Lopes <eduardo.rechelopes@exxonmobil.com>
2021-06-21 20:53:10 -0300

    add home screen

---
+++ src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import illustrationImage from '../assets/images/illustration.svg';
+import logoImage from '../assets/images/logo.svg';
+import googleIconImage from '../assets/images/google-icon.svg';
+
+import Button from '../components/Button';
+import '../styles/auth.scss';
+
+const Home: React.FC = () => {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImage} alt="Illustration for Q&A" />
+        <strong>Crie salas de Q&A ao-vivo</strong>
+        <p>Tire as dúvidas da audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImage} alt="letmeask logo" />
+          <button className="create-room">
+            <img src={googleIconImage} alt="Google logo"  />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input 
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
